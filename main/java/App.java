e079a5fb092f01d864a988a4d5ad7ee94af5325a
kodziarz <58184304+kodziarz@users.noreply.github.com>
2022-04-12 12:46:53 +0200

    Add files via upload

---
+++ main/java/App.java
@@ -0,0 +1,61 @@
+import com.fasterxml.uuid.Generators;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import controller.UserServiceImpl;
+import spark.Request;
+import spark.Response;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import model.User;
+
+public class App {
+
+    private static UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+    public static void main(String[] args) {
+        get("/api/users", (req, res) -> handleGetUsers(req, res));
+        get("/api/users/:id", (req, res) -> handleGetUser(req, res));
+        post("/api/users", (req, res) -> handleAddUser(req, res));
+    }
+
+
+    public static String handleGetUsers(Request req, Response res) {
+
+        Type listType = new TypeToken<HashMap<String, User>>(){}.getType();
+        Gson gson = new Gson();
+        return gson.toJson(userServiceImpl.getAllUsers(), listType);
+
+    }
+
+    public static String handleGetUser(Request req, Response res) {
+
+        String id = req.params("id");
+        Gson gson = new Gson();
+        return gson.toJson(userServiceImpl.getUser(id), User.class);
+
+    }
+
+    public static String handleAddUser(Request req, Response res) {
+
+        Gson gson = new Gson();
+        System.out.println("dostałem: " + req.body());
+        User user = gson.fromJson(req.body(), User.class);
+        user.generateId();
+        System.out.println("sparsowany obiekt: " + user.toString());
+
+        try {
+            userServiceImpl.addUser(user);
+            return "{\"status\":\"ok\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"status\":\"not ok\"}";
+        }
+    }
+}
---
+++ main/java/controller/UserService.java
@@ -0,0 +1,13 @@
+package controller;
+import model.User;
+
+import java.util.HashMap;
+
+public interface UserService {
+    void addUser(User user);
+    HashMap<String, User> getAllUsers();
+    User getUser(String id);
+    User editUser(User user);
+    void deleteUser(String id);
+    Boolean userExist(String id);
+}
